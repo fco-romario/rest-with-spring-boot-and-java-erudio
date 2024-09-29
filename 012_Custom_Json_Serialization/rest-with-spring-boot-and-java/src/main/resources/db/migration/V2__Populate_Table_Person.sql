25d0403b14bd7020840567fc00c7a609bb311f17
fco-romario <fco.romario.alves@gmail.com>
2024-09-29 19:54:00 -0300

    Revert "Criando Seção 12 JSON serialização customizada"
    This reverts commit 80c58bcaff00405266103c46f91487b24abbd7cf.

--- 012_Custom_Json_Serialization/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(6, 'São Paulo', 'Romário', 'Male', 'Alves'),
-	(8, 'FORTALEZA-CE', 'MARIA', 'Female', 'Alves'),
-	(9, 'FORTALEZA-CE', 'Leonardo', 'Male', 'davic'),
-	(14, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
-	(15, 'Smiljan- Croatia', 'Nikola', 'Male', 'Tesla'),
-	(16, 'Kentucky - US', 'Muhasdawammad', 'Male', 'Alif');
