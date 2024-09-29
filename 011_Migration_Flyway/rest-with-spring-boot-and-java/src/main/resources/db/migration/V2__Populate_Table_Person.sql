407a2d00addb777bfd7be81c119d9c02fcc846bc
fco-romario <fco.romario.alves@gmail.com>
2024-09-29 19:23:05 -0300

    Adicionando migrations à API

---
+++ 011_Migration_Flyway/rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(6, 'São Paulo', 'Romário', 'Male', 'Alves'),
+	(8, 'FORTALEZA-CE', 'MARIA', 'Female', 'Alves'),
+	(9, 'FORTALEZA-CE', 'Leonardo', 'Male', 'davic'),
+	(14, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(15, 'Smiljan- Croatia', 'Nikola', 'Male', 'Tesla'),
+	(16, 'Kentucky - US', 'Muhasdawammad', 'Male', 'Alif');
