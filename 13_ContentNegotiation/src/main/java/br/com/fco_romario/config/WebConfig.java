848790bd659ec682f4ce374a3c923f85da716105
fco-romario <fco.romario.alves@gmail.com>
2025-06-22 20:20:37 -0300

    Content Negotiation via HEADER Parameter

--- 13_ContentNegotiation/src/main/java/br/com/fco_romario/config/WebConfig.java
+++ 13_ContentNegotiation/src/main/java/br/com/fco_romario/config/WebConfig.java
@@ -13,9 +13,20 @@ public class WebConfig implements WebMvcConfigurer {
         // Via EXTENSION. http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/2.JSON depreciado em  Spring Boot 2.6
 
         // Via QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml  fica com a URL maior
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                    .mediaType("json", MediaType.APPLICATION_JSON)
+                    .mediaType("xml", MediaType.APPLICATION_XML)
+        */
+
+
+        // Via HEADR PARAM http://localhost:8080/api/person/v1/2
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
