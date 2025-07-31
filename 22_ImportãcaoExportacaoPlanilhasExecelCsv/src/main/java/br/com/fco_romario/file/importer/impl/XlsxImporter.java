4d35fdadfcaa21cedd819e6f8ac17c5e945f15ef
fco-romario <fco.romario.alves@gmail.com>
2025-07-30 21:13:40 -0300

    Implementando o importador de XLSX - Lendo Planilhas Excel

--- 22_ImportãcaoExportacaoPlanilhasExecelCsv/src/main/java/br/com/fco_romario/file/importer/impl/XlsxImporter.java
+++ 22_ImportãcaoExportacaoPlanilhasExecelCsv/src/main/java/br/com/fco_romario/file/importer/impl/XlsxImporter.java
@@ -2,14 +2,60 @@ package br.com.fco_romario.file.importer.impl;
 
 import br.com.fco_romario.data.dto.PersonDTO;
 import br.com.fco_romario.file.importer.contract.FileImporter;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
 
 public class XlsxImporter implements FileImporter {
 
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        return List.of();
+
+        try (XSSFWorkbook workbook = new XSSFWorkbook(inputStream)) {
+            XSSFSheet sheet = workbook.getSheetAt(0);// neste caso estou considerando a o xlsx so tem uma aba.caso haja mais, considerar mais de uma abas
+
+            Iterator<Row> rowIterator = sheet.iterator();
+
+            if(rowIterator.hasNext()) rowIterator.next(); // pula o cabeçalho e pega apenas as linhas
+
+            return parseRowsToPersonDTOList(rowIterator);
+        }
+    }
+
+    private List<PersonDTO> parseRowsToPersonDTOList(Iterator<Row> rowIterator) {
+        List<PersonDTO> people = new ArrayList<>();
+
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+            if(isRowValid(row)) {
+                people.add(parseRowToPersoDTO(row));
+            }
+        }
+
+        return people;
+    }
+
+    private PersonDTO parseRowToPersoDTO(Row row) {
+        PersonDTO person = new PersonDTO();
+        person.setFirstName(row.getCell(0).getStringCellValue());
+        person.setLastName(row.getCell(1).getStringCellValue());
+        person.setAddress(row.getCell(2).getStringCellValue());
+        person.setGender(row.getCell(3).getStringCellValue());
+        person.setEnabled(true);
+
+        return person;
+    }
+
+    private static boolean isRowValid(Row row) {
+        return row.getCell(0) != null && row.getCell(0).getCellType() != CellType.BLANK;
     }
 }
+
