f20bf86d6e14e7ce003a81f22efc14cd96436714
fco-romario <fco.romario.alves@gmail.com>
2025-08-05 14:17:55 -0300

    Criando Exceptions customizadas e o DTO de credenciais

--- 25_AutenticacaoJWTESpringSecurity/src/main/java/br/com/fco_romario/exception/handler/CustomizedEntityResponseHandler.java
+++ 25_AutenticacaoJWTESpringSecurity/src/main/java/br/com/fco_romario/exception/handler/CustomizedEntityResponseHandler.java
@@ -68,6 +68,15 @@ public class CustomizedEntityResponseHandler extends ResponseEntityExceptionHand
                 request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
 }
 
 
