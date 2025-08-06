f58ece6965bea0f829ffa01f391f62e7054812dd
fco-romario <fco.romario.alves@gmail.com>
2025-08-05 21:45:18 -0300

    Implementando o AuthenticationManager e o SecurityFilterChain

--- 25_AutenticacaoJWTESpringSecurity/src/main/java/br/com/fco_romario/config/SecurityConfig.java
+++ 25_AutenticacaoJWTESpringSecurity/src/main/java/br/com/fco_romario/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package br.com.fco_romario.config;
 
+import br.com.fco_romario.security.jwt.JwtTokenFilter;
 import br.com.fco_romario.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.beans.BeanProperty;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,5 +45,35 @@ public class SecurityConfig {
 
         passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
         return passwordEncoder;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)// 1. Desabilita autenticação básica (HTTP Basic Auth)
+                .csrf(AbstractHttpConfigurer::disable) // 2. Desabilita proteção CSRF (Cross-Site Request Forgery)
+                .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class) //Adiciona um filtro customizado ANTES do filtro de autenticação padrão do Spring
+                .sessionManagement( // 4. Define a política de sessão como STATELESS (sem sessão no servidor)
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                            .requestMatchers(
+                                    "/auth/signin", // Login
+                                    "/auth/refresh/**",      // Refresh token
+                                    "/auth/createUser",      // Criação de usuário (TODO: remover em produção)
+                                    "/swagger-ui/**",        // Swagger UI
+                                    "/v3/api-docs/**"        // Documentação OpenAPI
+                            ).permitAll()
+                        .requestMatchers("/api/**").authenticated()  // Rotas privadas (exigem autenticação)
+                        .requestMatchers("/users").denyAll() // Rotas bloqueadas (acesso negado mesmo com autenticação)
+                )
+                .cors(cors -> {}) //A classe WebConfig é quem definira as propriedade de Cors
+            .build();
     }
 }
