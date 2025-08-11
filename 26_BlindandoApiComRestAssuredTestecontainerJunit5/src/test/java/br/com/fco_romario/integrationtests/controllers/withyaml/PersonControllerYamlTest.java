8c2a286481fc439bb61e0ad247113a8900b9c533
fco-romario <fco.romario.alves@gmail.com>
2025-08-10 21:28:47 -0300

    Corrigindo os testes de Integração do Controller de Person com YAML

--- 26_BlindandoApiComRestAssuredTestecontainerJunit5/src/test/java/br/com/fco_romario/integrationtests/controllers/withyaml/PersonControllerYamlTest.java
+++ 26_BlindandoApiComRestAssuredTestecontainerJunit5/src/test/java/br/com/fco_romario/integrationtests/controllers/withyaml/PersonControllerYamlTest.java
@@ -2,7 +2,9 @@ package br.com.fco_romario.integrationtests.controllers.withyaml;
 
 import br.com.fco_romario.config.TestConfigs;
 import br.com.fco_romario.integrationtests.controllers.withyaml.mapper.YAMLMapper;
+import br.com.fco_romario.integrationtests.dto.AccountCredentialsDTO;
 import br.com.fco_romario.integrationtests.dto.PersonDTO;
+import br.com.fco_romario.integrationtests.dto.TokenDTO;
 import br.com.fco_romario.integrationtests.dto.wrappers.xmlYaml.person.PagedModelPerson;
 import br.com.fco_romario.integrationtests.testcontainers.AbstractIntegrationTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -31,25 +33,50 @@ class PersonControllerYamlTest extends AbstractIntegrationTest {
     private static YAMLMapper objectMapper;
 
     private static PersonDTO person;
+    private static TokenDTO tokenDTO;
 
     @BeforeAll  //uma instância para todo a class diferente do @BeforeEach
     static void setUp() {
         objectMapper = new YAMLMapper();
         person = new PersonDTO();
+        tokenDTO = new TokenDTO();
+    }
+
+    @Test
+    @Order(0)
+    void signin() {
+        AccountCredentialsDTO credentials = new AccountCredentialsDTO("leandro", "admin123");
+
+        tokenDTO = given()
+                .basePath("/auth/signin")
+                .port(TestConfigs.SERVER_PORT)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(credentials)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(TokenDTO.class);
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_EXEMPLO)
+                .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenDTO.getRefreshToken())
+                .setBasePath("/api/person/v1")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL)) // Mostra log to que esta vindo
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL)) // Mostra log to que esta indo
+                .build();
+
+        assertNotNull(tokenDTO.getAccessToken());
+        assertNotNull(tokenDTO.getRefreshToken());
     }
 
     @Test
     @Order(1)
     void createTest() throws JsonProcessingException {
         mockPerson();
-
-        specification = new RequestSpecBuilder()
-                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_EXEMPLO)
-                .setBasePath("/api/person/v1")
-                .setPort(TestConfigs.SERVER_PORT)
-                    .addFilter(new RequestLoggingFilter(LogDetail.ALL)) // Mostra log to que esta vindo
-                    .addFilter(new ResponseLoggingFilter(LogDetail.ALL)) // Mostra log to que esta indo
-                .build();
 
         var createdPerson = given().config(
                 RestAssuredConfig.config()
@@ -282,5 +309,7 @@ class PersonControllerYamlTest extends AbstractIntegrationTest {
         person.setAddress("Helsinki - Finland");
         person.setGender("Male");
         person.setEnabled(true);
+        person.setProfileUrl("https://github.com/fco-romario");
+        person.setPhotoUrl("https://github.com/fco-romario/rest-with-spring-boot-and-java-erudio");
     }
 }
